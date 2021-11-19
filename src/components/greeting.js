119b5b298c5b96382dd8c34e81cfbaf08e882a53
Janco Sambaer <janco.sambaer@student.ap.be>
2021-11-19 08:01:53 +0100

    taaken vakantie deel

---
+++ src/components/greeting.js
@@ -0,0 +1,6 @@
+// Defineren van de <Greeting> component
+const Greeting = (props) => {
+    return (
+      <p>Hi {props.name}!</p>
+    )
+  }
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,51 +5,25 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+ import * as React from 'react'
+ import { Link } from 'gatsby'
+ 
+ const Layout = ({ pageTitle, children }) => {
+   return (
+     <div>
+       <title>{pageTitle}</title>
+       <nav>
+         <ul>
+           <li><Link to="/">Home</Link></li>
+           <li><Link to="/about">About</Link></li>
+         </ul>
+       </nav>
+       <main>
+         <h1>{pageTitle}</h1>
+         {children}
+       </main>
+     </div>
+   )
+ }
+ 
+ export default Layout
